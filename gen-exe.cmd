826842582dba446773dbe4205fa0107a594b1708
lsby <2451759073@qq.com>
2026-03-01 17:18:14 +0800

    增加编译命令

---
+++ gen-exe.cmd
@@ -0,0 +1,1 @@
+dotnet publish src/Qsp.FSharp.Cli/Qsp.FSharp.Cli.fsproj -c Release -r win-x64 --self-contained true -p:PublishSingleFile=true --framework netcoreapp3.1
---
+++ run-by-exe.cmd
@@ -0,0 +1,1 @@
+src\Qsp.FSharp.Cli\bin\Release\netcoreapp3.1\win-x64\publish\Qsp.FSharp.Cli.exe parse data\beggar.txt -o data\beggar_ast.json
